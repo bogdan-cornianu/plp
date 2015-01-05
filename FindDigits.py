ae09797f523bad40270d6761362bfccbccc343d0
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2015-01-05 11:12:56 +0200

    code review changes.

--- FindDigits.py
+++ FindDigits.py
@@ -3,12 +3,12 @@ from sys import stdin
 
 number_of_cases = int(stdin.readline())
 
-for line in range(0, number_of_cases):
+for line in range(number_of_cases):
     number = stdin.readline()
     counter = 0
 
-    for digit in list(str(number).strip()):
-        if int(digit) > 0 and int(number) % int(digit) == 0:
+    for digit in str(number).strip():
+        if int(digit) and int(number) % int(digit) == 0:
             counter += 1
 
     print counter

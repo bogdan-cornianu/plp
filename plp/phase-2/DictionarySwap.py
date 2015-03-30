8c34f3af28b44a01387a6346e7bdacb9d3162f4f
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2015-03-30 18:19:28 +0300

    Solve swap of dictionary key - values.

---
+++ plp/phase-2/DictionarySwap.py
@@ -0,0 +1,34 @@
+__author__ = 'bogdan.cornianu'
+
+import sys
+
+try:
+    input_dict = input('Input: ')
+
+except NameError:
+        print 'Not a dictionary.'
+        sys.exit(0)
+
+
+new_dict = {}
+
+
+def is_tuple_assignable_as_key(t):
+    for item in t:
+        if type(item) not in [str, int]:
+            return False
+
+    return True
+
+for k, v in input_dict.iteritems():
+    type_of_value = type(v)
+
+    if type_of_value in [str, int]:
+        new_dict[v] = k
+
+    elif type_of_value is tuple and is_tuple_assignable_as_key(v) is True:
+        new_dict[v] = k
+
+
+print 'Output: '
+print new_dict

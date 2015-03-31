73f7e5d74c877585db19228429ceeeb1f36c59d4
Bogdan Cornianu <bogdan.cornianu@3pillarglobal.com>
2015-03-31 15:39:02 +0300

    add threshold argument.

--- plp/phase-2/FunctionDecorator.py
+++ plp/phase-2/FunctionDecorator.py
@@ -2,18 +2,21 @@ __author__ = 'bogdan.cornianu'
 import time
 
 
-def time_slow(func):
-    def timer(*args, **kwargs):
-        time_start = time.time()
-        result = func(*args, **kwargs)
-        time_end = time.time()
-        print 'Execution took ' + str(time_end - time_start) + ' seconds for function ' + func.__name__
+def time_slow(threshold):
+    def inner_dec(func):
+        def timer(*args, **kwargs):
+            time_start = time.time()
+            result = func(*args, **kwargs)
+            time_end = time.time()
+            print 'Execution took ' + str(time_end - time_start) + ' seconds for function ' + func.__name__  + \
+                  ' with a threshold of ' + str(threshold)
 
-        return result
-    return timer
+            return result
+        return timer
+    return inner_dec
 
 
-@time_slow
+@time_slow(threshold=0.09)
 def my_func(a, b='slow'):
     print a, b
     time.sleep(1)
